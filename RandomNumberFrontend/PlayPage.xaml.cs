2c484b882e3cee7221d2b22abf3f9f9579c0a855
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-12 00:43:12 +0300

    play

--- RandomNumberFrontend/PlayPage.xaml.cs
+++ RandomNumberFrontend/PlayPage.xaml.cs
@@ -6,4 +6,16 @@ public partial class PlayPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnPlayClicked(object sender, EventArgs e)
+    {
+        var myNumber = MyNumberEntry.Text;
+
+        var response = await Server.Play(User.Nickname, myNumber);
+
+        var responseSuccess = response.Item1;
+        var responseString = response.Item2;
+
+        PlayResult.Text = $"[{responseSuccess}] {responseString}";
+    }
 }
