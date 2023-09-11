ac4998f6b28b2c1fa7103a4a3050c194b09d3a90
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-11 22:28:13 +0300

    Server

---
+++ RandomNumberFrontend/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Http;
+
+namespace RandomNumberFrontend
+{
+	public class Server
+	{
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<bool> Registration(string nickname, string password)
+        {
+            var response = await client.PostAsync($"http://localhost:7071/api/Registration?nickname={nickname}&password={password}", null);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
