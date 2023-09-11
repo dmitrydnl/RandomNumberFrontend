d8397f21fddeb64e68e028ef467ca754a83ce497
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-11 21:47:54 +0300

    init

---
+++ RandomNumberFrontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomNumberFrontend;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ RandomNumberFrontend/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomNumberFrontend;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
