197e7d7277676136774eb1b98e0df77d8d31214f
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-11 23:03:49 +0300

    MyGames

--- RandomNumberFrontend/StatisticsPage.xaml.cs
+++ RandomNumberFrontend/StatisticsPage.xaml.cs
@@ -2,11 +2,15 @@
 
 public partial class StatisticsPage : ContentPage
 {
-	public StatisticsPage()
+    public List<int> MyGames { get; set; }
+
+    public StatisticsPage()
 	{
 		InitializeComponent();
 		Welcome.Text = $"Welcome {User.Nickname}";
-	}
+        MyGames = new List<int> { 123, 23, 1, 90 };
+        BindingContext = this;
+    }
 
     private void OnPlayClicked(object sender, EventArgs e)
     {
