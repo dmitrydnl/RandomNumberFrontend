940dbb2d1c89cdc1ab82e536943fac1bfb51815f
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-11 22:41:01 +0300

    Update Server.cs

--- RandomNumberFrontend/Server.cs
+++ RandomNumberFrontend/Server.cs
@@ -6,10 +6,18 @@ namespace RandomNumberFrontend
 	public class Server
 	{
         private static readonly HttpClient client = new HttpClient();
+        private static readonly string prefix = "http://localhost:7071/api";
+
+        public static async Task<(bool, string)> Authorization(string nickname, string password)
+        {
+            var response = await client.PostAsync($"{prefix}/Authorization?nickname={nickname}&password={password}", null);
+            var responseString = await response.Content.ReadAsStringAsync();
+            return (response.IsSuccessStatusCode, responseString);
+        }
 
         public static async Task<(bool, string)> Registration(string nickname, string password)
         {
-            var response = await client.PostAsync($"http://localhost:7071/api/Registration?nickname={nickname}&password={password}", null);
+            var response = await client.PostAsync($"{prefix}/Registration?nickname={nickname}&password={password}", null);
             var responseString = await response.Content.ReadAsStringAsync();
             return (response.IsSuccessStatusCode, responseString);
         }
