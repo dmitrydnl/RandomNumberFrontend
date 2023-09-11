67a71a38f2ec79e2f9967f97a04122a5af1ccfea
Dmitry Danilin <d.danilin@fenix-bg.ru>
2023-09-11 22:41:17 +0300

    Update MainPage.xaml.cs

--- RandomNumberFrontend/MainPage.xaml.cs
+++ RandomNumberFrontend/MainPage.xaml.cs
@@ -7,13 +7,18 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnAuthorizationClicked(object sender, EventArgs e)
+	private async void OnAuthorizationClicked(object sender, EventArgs e)
 	{
 		var nickname = NicknameEntry.Text;
         var password = PasswordEntry.Text;
 
-        Console.WriteLine($"Authorization {nickname} {password}");
-	}
+        var response = await Server.Authorization(nickname, password);
+
+        var responseSuccess = response.Item1;
+        var responseString = response.Item2;
+
+        AuthRegResult.Text = $"[{responseSuccess}] {responseString}";
+    }
 
     private async void OnRegistrationClicked(object sender, EventArgs e)
     {
